f38b6f9beb9d5ce2b84044333242c85091c86d70
Vinicius <vgomesfalcao@gmail.com>
2021-06-13 07:37:41 -0300

    Maps implementation

--- app/src/main/java/com/example/bloqueador_telemarketing/ui/activity/AppActivity.kt
+++ app/src/main/java/com/example/bloqueador_telemarketing/ui/activity/AppActivity.kt
@@ -23,3 +23,4 @@ class AppActivity : FragmentActivity() {
 
 
 
+//
---
+++ app/src/main/java/com/example/bloqueador_telemarketing/ui/fragment/MapFragment.kt
@@ -0,0 +1,36 @@
+package com.example.bloqueador_telemarketing.ui.fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.bloqueador_telemarketing.R
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
+
+class MapFragment : Fragment() {
+
+    private val callback = OnMapReadyCallback { googleMap ->
+        val brasilia = LatLng(-15.834963163926998, -47.91285886006427)
+        googleMap.addMarker(MarkerOptions().position(brasilia).title("Marcador Brasília"))
+        googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(brasilia, 15f))
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_map, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
+        mapFragment?.getMapAsync(callback)
+    }
+}
